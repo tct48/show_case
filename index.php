0f0f089fd808073092ccb84f1c9e8b8aabd74d78
ketarWorapol <48780928+ketarWorapol@users.noreply.github.com>
2021-04-07 20:14:14 +0700

    new cv

--- index.php
+++ index.php
@@ -1,14 +1,217 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <style>
+        body,
+        html {
+            /* height: 100%; */
+            margin: 0px;
+        }
+
+        .container {
+            width: 800px;
+            height: 100%;
+            margin: 0px auto;
+            background-color: white;
+            padding: 30px 80px 30px;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        h1,
+        h3 {
+            font-family: 'Oswald', sans-serif;
+            margin-bottom: 0px;
+        }
+
+        h3 {
+            text-decoration: underline;
+        }
+
+        table,
+        p {
+            font-family: 'Lato', sans-serif;
+        }
+
+        .m-0 {
+            margin: 0px;
+        }
+
+        td {
+            padding-right: 30px;
+        }
+
+        .content p {
+            margin: 0px;
+        }
+    </style>
 </head>
-<body>
-    <h1>Worapol Mahachai</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, assumenda. Doloribus quaerat quasi quam. Fuga aperiam expedita, asperiores provident, cumque itaque nihil ipsum a ratione maiores reprehenderit minima cupiditate quam?</p>
-    
+
+<body style="background: rgb(230,230,230);">
+    <div class="container">
+        <div class="center">
+            <h1>Worapol Mahachai (Tar)</h1>
+            <p>10/30 Village No.6 Malaiman Road, Wangtaku Sub-district,<br>Muang District, Nakhon Pathom, 73000</p>
+            <p>E-mail : tar_solo@outlook.co.th, Tel : 091-115-7859 <br>
+                Github : https://github.com/tct48
+            </p>
+        </div>
+
+        <div class="content">
+            <h3>Objective</h3>
+            <p class="m-0">To apply for a position of computer programmer or related.</p>
+
+            <h3>Personal Details</h3>
+            <table border="0">
+                <tr>
+                    <td>Date of birth</td>
+                    <td>:<span style="margin-left: 15px;">&nbsp;</span> 22 Novermber 1997</td>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                    <td>:<span style="margin-left: 15px;">&nbsp;</span> 23 years</td>
+                </tr>
+                <tr>
+                    <td>
+                        Nationality
+                    </td>
+                    <td>
+                        :<span style="margin-left: 15px;">&nbsp;</span> Thai
+                    </td>
+                </tr>
+                <tr>
+                    <td>Religion</td>
+                    <td>:<span style="margin-left: 15px;">&nbsp;</span> Buddhism</td>
+                </tr>
+                <tr>
+                    <td>Personality</td>
+                    <td>:<span style="margin-left: 15px;">&nbsp;</span> Fast learner, responsible, friendly and eager to
+                        learn new technology.</td>
+                </tr>
+                <tr>
+                    <td>Language</td>
+                    <td>:<span style="margin-left: 15px;">&nbsp;</span> English (Intermediate)</td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="content">
+            <h3>Education</h3>
+            <p>
+                Nakhon Pathom Vocational College (2016 – 2017)<br>
+                Vocational Diploma in Information Technology<br>
+                GPA: 3.75<br>
+                King Mongkut's University of Technology North Bangkok (2018 - 2020)<br>
+                Bachelor of Science in Technical Education Program in Computer Technology<br>
+                GPA: 3.32
+            </p>
+        </div>
+
+        <div class="content">
+            <h3>Technical Skills</h3>
+            <p>
+                Programming : C#, C, Typescript, HTML5, CSS3, SQL, PHP, VB.NET<br>
+                Framework & Libraries: Angular, MEAN Stack (basic), Yii (basic), JWT, Bootstrap, Bulma<br>
+                Database: MySQL, Firebase, MongoDB<br>
+                tools: Git Version Control, NodeJS, Heroku
+            </p>
+        </div>
+
+        <div class="content">
+            <h3>Work Experience</h3>
+            <table border="0">
+                <tr>
+                    <td style="vertical-align: top;">March 2020 - April 2021</td>
+                    <td>:<span style="margin-left: 15px;">&nbsp;</span>Nakhon Pathom Vocational College<br><span
+                            style="margin-left: 15px;">&nbsp;
+                            Practice Teacher<br><span style="margin-left: 15px;">&nbsp;
+                                Teaching Course : Introduction to computer graphic,<br><span
+                                    style="margin-left: 15px;">&nbsp;
+                                    Program development with visual basic language
+                    </td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top;">2016 - Present</td>
+                    <td>:<span style="margin-left: 15px;">&nbsp;</span>Web Application with Angular7 and
+                        DBA(MySQL),<br><span style="margin-left: 15px;">&nbsp;
+                            Vehicle inspection facility, Pathumthani<br><span style="margin-left: 15px;">&nbsp;
+                                Web Developer
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="content">
+            <h3>Work Experience</h3>
+            <p>
+                Company: Freelance<br>
+                Period: May 2016 – Present<br>
+                Position: Web Developer
+            </p>
+
+            <h3>
+                Projects Reference
+            </h3>
+            <ul>
+                <li>
+                    Project: Vehicle inspection facility (Pathumthani)<br>
+                    Responsibilities:
+                    <ul>
+                        <li>Design and Analyze business requirements.</li>
+                        <li>System design.</li>
+                        <li>Design database.</li>
+                        <li>Develop web application with Angular7 and DBA (MySQL)</li>
+                        <li>Application to manage CRUD information in database.</li>
+                    </ul>
+                </li>
+                <li>
+                    • Project: Subpodsawat Co., Ltd. (Nakhon Pathom)<br>
+                    Responsibilities:
+                    <ul>
+                        <li>Design and Analyze business requirements.</li>
+                        <li>System design.</li>
+                        <li>Design database.</li>
+                        <li>Develop Windows Forms App (.NET Framework)</li>
+                        <li>Application to manage CRUD information in database.</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div class="content">
+            <h3>Work Experience</h3>
+            <p>
+                Company: King Mongkut's University of Technology North Bangkok<br>
+                Period: May 2020 – March 2021<br>
+                Position: Senior Project
+            </p>
+
+            <h3>
+                Projects Reference
+            </h3>
+            <ul>
+                <li>
+                    Project: Learning Management System<br>
+                    Responsibilities:
+                    <ul>
+                        <li>Develop web application with MEAN Stack.</li>
+                        <li>Testing.</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
 </body>
+
 </html>
